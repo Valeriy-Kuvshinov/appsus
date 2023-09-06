9485486715866a8bfe941104f86679d4427074b4
Valeriy-Kuvshinov <valerykuvshinuv@gmail.com>
2023-09-06 16:36:09 +0300

    mail to note intergration

--- apps/mail/cmps/MailPreview.jsx
+++ apps/mail/cmps/MailPreview.jsx
@@ -1,36 +1,45 @@
 
 // const { useState , useEffect} = React
+import { EmailService } from "../services/mail.service.js"
 
 const { useParams, useNavigate, Link } = ReactRouterDOM
 
-export function MailPreview({email}){
-    // console.log(email)
-    // const [emails, getEmails] = useState(email)
-    
-    function changeStarSelection(ev){
-        // console.log(emails.isStar)
-        // if (emails.isStar===true) {
-        //     emails.isStar=false
-        // } else {
-        //     emails.isStar=true
-        // }
-        // getEmails(emails)
-    }
-    return <section className="mail">
-            
-            <div className="innerBox">
-              <button onClick={changeStarSelection}>
-                {/* {console.log(emails.isStar)} */}
-                {(email.isStar===true)?
-                <i className="fa-solid fa-star"></i>:
-                <i className="fa-regular fa-star"></i>}
-              </button>
-              <button>X</button>
-              <button>X</button>
-              <p>{email.from}</p>
-            </div>
-              <p>{email.subject}</p>
-              <p>{email.sentAt}</p>
-              {/* <Link to={`/mail/details/${email.id}`}>S</Link> */}
-           </section>
+export function MailPreview({ email }) {
+  // console.log(email)
+  // const [emails, getEmails] = useState(email)
+
+  function changeStarSelection(ev) {
+    // console.log(emails.isStar)
+    // if (emails.isStar===true) {
+    //     emails.isStar=false
+    // } else {
+    //     emails.isStar=true
+    // }
+    // getEmails(emails)
+  }
+
+  const convertToNote = () => {
+    EmailService.createNoteFromEmail(email.id)
+      .then(() => {
+        console.log("Converted to note")
+      })
+  }
+
+  return <section className="mail">
+
+    <div className="innerBox">
+      <button onClick={changeStarSelection}>
+        {/* {console.log(emails.isStar)} */}
+        {(email.isStar === true) ?
+          <i className="fa-solid fa-star"></i> :
+          <i className="fa-regular fa-star"></i>}
+      </button>
+      <button onClick={convertToNote}> OOO </button>
+      <button>X</button>
+      <p>{email.from}</p>
+    </div>
+    <p>{email.subject}</p>
+    <p>{email.sentAt}</p>
+    {/* <Link to={`/mail/details/${email.id}`}>S</Link> */}
+  </section>
 }
