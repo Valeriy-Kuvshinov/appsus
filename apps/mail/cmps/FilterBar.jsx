802bf9f281dfad3a2d8b4562ce1e3fdaaae415b8
RoeiTal123 <roeit1977@gmail.com>
2023-09-07 14:19:28 +0300

    small changes

--- apps/mail/cmps/FilterBar.jsx
+++ apps/mail/cmps/FilterBar.jsx
@@ -1,12 +1,28 @@
 
-export function FilterBar(){
+import { EmailService } from "../services/mail.service.js"
+
+const { useState, useEffect } = React
+
+export function FilterBar({val,func}){
+    
+    const [emails, filterEmails] = useState([])
+    var filter
     function onFilterChange(type){
-        console.log(type)
+        filter=type
+        EmailService.setFilterBy2(type)
+        console.log(filter)
+        func(val)
+        EmailService.query().then(console.log)
     }
+    useEffect(() => {
+        console.log('test')
+        EmailService.query().then(emails => filterEmails(emails))
+    }, [filter])
+    console.log(emails)
 
     return <select onChange={(e) => onFilterChange(e.target.value)}>
-    <option value="all">All</option>
-    <option value="Date">Date</option>
-    <option value="Name">Name</option>
-</select>
+              <option value="all">All</option>
+              <option value="Date">Date</option>
+              <option value="Subject">Subject</option>
+           </select>
 }
--- apps/mail/cmps/MailList.jsx
+++ apps/mail/cmps/MailList.jsx
@@ -1,6 +1,6 @@
 import { EmailService } from "../services/mail.service.js"
 import { MailPreview } from "./MailPreview.jsx"
-
+import { FilterBar } from "./FilterBar.jsx"
 
 
 const { useState, useEffect } = React
@@ -9,11 +9,13 @@ export function MailList() {
     const [emails, getEmails] = useState([])
 
     useEffect(() => {
+        console.log('test')
         EmailService.query().then(emails => getEmails(emails))
     }, [])
     
     if (emails.length === 0) return
-    return <section className="mails">
+    return <section className="mails"> 
+        <FilterBar val={emails} func={getEmails}/>
         <ul className="mail-line">
             {emails.map(mail =>
                 <li key={mail.id} className="mail-box">
