f4318b4b9e12d3dd3c75a46d6123bd56ae717cfd
Valeriy-Kuvshinov <valerykuvshinuv@gmail.com>
2023-09-09 15:19:12 +0300

    small changes to home page

--- views/Home.jsx
+++ views/Home.jsx
@@ -13,7 +13,8 @@ export function Home() {
                         <img className='project-logo' src='imgs/mister-mail.jpg' alt='mails logo' />
                         <div className='project-info'>
                             <h2>Mr. Mail</h2>
-                            <h4>Looking for an alternative to Gmail or Hotmail, have no fear, Mr. Mail is here!</h4>
+                            <h4>Looking for an alternative to Gmail or Hotmail?</h4>
+                            <h4>Have no fear, Mr. Mail is here!</h4>
                         </div>
                         <img className='project-showcase' src='imgs/mails.png' alt='mails showcase' />
                     </div>
@@ -23,7 +24,8 @@ export function Home() {
                         <img className='project-logo' src="imgs/miss-notes.jpg" alt='notes logo' />
                         <div className='project-info'>
                             <h2>Ms. Keep</h2>
-                            <h4>It is always good to take a note of what you have to do, thankfully Ms. Keep does the job!</h4>
+                            <h4>It is always good to take a note of what you gotta do.</h4>
+                            <h4>Thankfully, Ms. Keep can do the job!</h4>
                         </div>
                         <img className='project-showcase' src="imgs/notes.png" alt='notes showcase' />
                     </div>
@@ -33,7 +35,8 @@ export function Home() {
                         <img className='project-logo' src='imgs/miss-book.jpg' alt='books logo' />
                         <div className='project-info'>
                             <h2>Ms. Books</h2>
-                            <h4>If you ever wanted to read a book outside of Amazon, look no further, and go to Ms. Books!</h4>
+                            <h4>For all the book lovers, we got the perfect site for you.</h4>
+                            <h4>Grab your favorite book, from our lovely shop, Ms. Books!</h4>
                         </div>
                         <img className='project-showcase' src='imgs/books.png' alt='books showcase' />
                     </div>
