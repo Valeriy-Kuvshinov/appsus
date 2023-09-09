c26597d5eae556135ea35e67e6d6421ada0db6be
Valeriy-Kuvshinov <valerykuvshinuv@gmail.com>
2023-09-09 21:53:19 +0300

    about design

--- views/About.jsx
+++ views/About.jsx
@@ -1,36 +1,36 @@
 
 export function About() {
 
-    var currentStep=1
+    var currentStep = 1
 
-    function clicked(ev){
-        const id=ev.target.id
-        if(id==='1'&&currentStep===1){
+    function clicked(ev) {
+        const id = ev.target.id
+        if (id === '1' && currentStep === 1) {
             alert('read closely')
             alert('you have entered a classified area')
             alert('if you want to get out alive you must solve my riddle')
             alert('there are 15 steps until you are safe')
             alert('good luck')
-            currentStep=2
+            currentStep = 2
         }
-        else if(id==='1'&&currentStep===2){
+        else if (id === '1' && currentStep === 2) {
             alert('whats the first letter of the alphabet?')
         }
 
-        if(id==='2'&&currentStep===2){
+        if (id === '2' && currentStep === 2) {
             document.getElementById('1').classList.toggle('hidden')
             alert('correct')
             alert('it seems you are smarter then the average person')
             alert('which doesnt mean much but still')
             alert('the next step wont be as easy')
-            currentStep=3
+            currentStep = 3
             document.getElementById('3').classList.toggle('hidden')
             document.getElementById('3.1').classList.toggle('hidden')
             alert('who is the number one youtuber?')
         }
-        if(id==='3.1'&&currentStep===3){
-            const answer=(document.getElementById('3').value).toLowerCase()
-            if(answer==='pewdiepie'){
+        if (id === '3.1' && currentStep === 3) {
+            const answer = (document.getElementById('3').value).toLowerCase()
+            if (answer === 'pewdiepie') {
                 document.getElementById('3').classList.toggle('hidden')
                 document.getElementById('3.1').classList.toggle('hidden')
                 alert('correct')
@@ -38,28 +38,28 @@ export function About() {
                 alert('or you are using the internet for answers')
                 alert('either way it wont help you')
                 alert('onto the next step')
-                currentStep=4
+                currentStep = 4
                 document.getElementById('4.1').classList.toggle('hidden')
                 document.getElementById('4.2').classList.toggle('hidden')
                 alert('which one is correct?')
             }
         }
-        if((id==='4.1'||id==='4.2')&&currentStep===4){
-           if(id==='4.1'){
-            alert('correct')
-            alert('its common knowledge afterall')
-            alert('it seems you wont give up')
-            alert('then so am i!')
-            alert('next step!')
-            currentStep=5
-            document.getElementById('4.1').classList.toggle('hidden')
-            document.getElementById('4.2').classList.toggle('hidden')
-            document.getElementById('5').classList.toggle('hidden')
-           } else {
-            alert('WRONG!')
-           }
+        if ((id === '4.1' || id === '4.2') && currentStep === 4) {
+            if (id === '4.1') {
+                alert('correct')
+                alert('its common knowledge afterall')
+                alert('it seems you wont give up')
+                alert('then so am i!')
+                alert('next step!')
+                currentStep = 5
+                document.getElementById('4.1').classList.toggle('hidden')
+                document.getElementById('4.2').classList.toggle('hidden')
+                document.getElementById('5').classList.toggle('hidden')
+            } else {
+                alert('WRONG!')
+            }
         }
-        if(id==='5.1'&&currentStep===5){
+        if (id === '5.1' && currentStep === 5) {
             document.getElementById('5').classList.toggle('hidden')
             alert('correct')
             alert('you have a keen eye')
@@ -67,44 +67,79 @@ export function About() {
             alert('or a job, but i dont judge')
             alert('next step!')
             document.getElementById('6').classList.toggle('hidden')
-            currentStep=6
+            currentStep = 6
         }
-        if(id==='6'&&currentStep===6){
-            console.log( document.getElementById('6').value)
+        if (id === '6' && currentStep === 6) {
+            console.log(document.getElementById('6').value)
         }
     }
-    return <section className="about">
-              <h1>About P<span onClick={clicked} id='2' className="button2">a</span>ge</h1>
-              <button onClick={clicked} id='1' className="button1">Click Me</button>
-              <input type="text" id="3" className='hidden'></input>
-              <button onClick={clicked} id="3.1" className='hidden'>Submit Answer</button>
-              <button onClick={clicked} id="4.1" className='hidden'>gif</button>
-              <button onClick={clicked} id="4.2" className='hidden'>jif</button>
-              <h1 id='5' className='hidden'>888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
-              88888888888888888888888888888888888888<span onClick={clicked} id='5.1' className='fifth'>3</span>888888888888888888888888888888888888888888888888888888888888
-              888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
-              888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
-              888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
-              888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
-              888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
-              888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
-              888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
-              888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
-              888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
-              888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
-              888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
-              888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
-              888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
-              888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
-              888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
-              888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
-              888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
-              888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
-              888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
-              888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
-              888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
-              888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888</h1>
+    return <section className="about main-layout">
+        <div className='about-text'>
+            <h1>About Us</h1>
+            <h4>We are a team of young developers who aspire to join high-tech industry to gain experience and change the world for the better.</h4>
+            <h4>Currently we are students of Coding Academy, however soon we shall we shall enter the work force, filled with bright ideas!</h4>
+            <h4>Day by day, we work on new projects and only gain further knowledge in FullStack Development </h4>
+            <h4>We welcome you all, to take a look at our work on GitHub!</h4>
+            <br />
+            <h1>Meet The Team</h1>
+            <div className='team-layout'>
+                <div className='team-member'>
+                    <h2>Roei Tal</h2>
+                    <h4>Main role: handling the logic</h4>
+                    <h4>Follow me on:</h4>
+                    <div className='team-links'>
+                        <h4><a href="https://github.com/RoeiTal123"><i className="fa-brands fa-square-github"></i></a></h4>
+                        <h4><a href="https://github.com/RoeiTal123"><i className="fa-brands fa-linkedin"></i></a></h4>
+                        <h4><a href="https://github.com/RoeiTal123"><i className="fa-brands fa-square-instagram"></i></a></h4>
+                    </div>
+                    <img src="imgs/roei.png" />
+                </div>
+                <div className='team-member'>
+                    <h2>Valeriy Kuvshinov</h2>
+                    <h4>Main role: handling the design</h4>
+                    <h4>Follow me on:</h4>
+                    <div className='team-links'>
+                        <h4><a href="https://github.com/Valeriy-Kuvshinov"><i className="fa-brands fa-square-github"></i></a></h4>
+                        <h4><a href="https://www.linkedin.com/in/valeriy-kuvshinov-85b14b282/"><i className="fa-brands fa-linkedin"></i></a></h4>
+                        <h4><a href="https://www.instagram.com/valeriy.kuvshin.ov/"><i className="fa-brands fa-square-instagram"></i></a></h4>
+                    </div>
+                    <img src="imgs/valeriy.png" />
+                </div>
+            </div>
+        </div>
+        <section className='secret'>
+            <h1>About P<span onClick={clicked} id='2' className="button2">a</span>ge</h1>
+            <button onClick={clicked} id='1' className="button1">Click Me</button>
+            <input type="text" id="3" className='hidden'></input>
+            <button onClick={clicked} id="3.1" className='hidden'>Submit Answer</button>
+            <button onClick={clicked} id="4.1" className='hidden'>gif</button>
+            <button onClick={clicked} id="4.2" className='hidden'>jif</button>
+            <h1 id='5' className='hidden'>888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+                88888888888888888888888888888888888888<span onClick={clicked} id='5.1' className='fifth'>3</span>888888888888888888888888888888888888888888888888888888888888
+                888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+                888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+                888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+                888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+                888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+                888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+                888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+                888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+                888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+                888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+                888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+                888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+                888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+                888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+                888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+                888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+                888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+                888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+                888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+                888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+                888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+                888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888</h1>
 
-              <input type="range" id="6" step={1} min={1} max={6} onChange={clicked} className='hidden'/>
-           </section>
+            <input type="range" id="6" step={1} min={1} max={6} onChange={clicked} className='hidden' />
+        </section>
+    </section>
 }
